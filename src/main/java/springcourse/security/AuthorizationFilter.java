b9aca892becd1a16c66371ae035ee9a3483ef891
Carnegie <carnegiejunior@gmail.com>
2020-06-16 23:23:32 -0300

    AuthrizationFilter refactor

--- src/main/java/springcourse/security/AuthorizationFilter.java
+++ src/main/java/springcourse/security/AuthorizationFilter.java
@@ -1,7 +1,6 @@
 package springcourse.security;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -21,6 +20,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
@@ -28,59 +28,50 @@ import springcourse.infraestructure.resource.exceptions.ApiError;
 
 public class AuthorizationFilter extends OncePerRequestFilter {
 
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
-		
 		String jwt = request.getHeader(HttpHeaders.AUTHORIZATION);
 	
 		if (jwt == null || !jwt.startsWith(SecurityConstants.JWT_PROVIDER)) {
-			ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED.value(), SecurityConstants.JWT_INVALID_TOKEN_MSG, new Date());
-			PrintWriter writer = response.getWriter();
-			
-			ObjectMapper mapper = new ObjectMapper();
-			String apiErrorString = mapper.writeValueAsString(apiError);
-			writer.write(apiErrorString);
-			
-			response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			
+			errorResult(response, SecurityConstants.JWT_INVALID_TOKEN_MSG);
 			return;
 		}
 		
 		jwt = jwt.replace(SecurityConstants.JWT_PROVIDER, "");
 		
 		try {
+
 			Claims claims = new JwtManager().parseToken(jwt);
-			String subject = claims.getSubject();
-			List<String> roles = (List<String>) claims.get(SecurityConstants.JWT_ROLE_KEY);
+			
+			@SuppressWarnings("unchecked")
+			List<String> roles =
+				(List<String>) claims.get(SecurityConstants.JWT_ROLE_KEY);
 			
 			List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+			
 			roles.forEach(role->{
 				grantedAuthorities.add(new SimpleGrantedAuthority(role));
 			});
 			
-			Authentication authentication = new UsernamePasswordAuthenticationToken(subject, null, grantedAuthorities);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(claims.getSubject(), null, grantedAuthorities);
 			
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 			
 		} catch (Exception e) {
-			ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED.value(), e.getMessage(), new Date());
-			PrintWriter writer = response.getWriter();
-			
-			ObjectMapper mapper = new ObjectMapper();
-			String apiErrorString = mapper.writeValueAsString(apiError);
-			writer.write(apiErrorString);
-			
-			response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			
+			errorResult(response, e.getMessage());
 			return;		
 		}
 		
 		filterChain.doFilter(request, response);
+	}
+
+	private void errorResult(HttpServletResponse response, String msg) throws IOException, JsonProcessingException {
+		ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED.value(), msg, new Date());
+		response.getWriter().write((new ObjectMapper()).writeValueAsString(apiError));
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
 	}
 
 }
